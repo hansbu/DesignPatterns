feab6b83d0b759dd90a1855d34db81e4cdc4ca6b
hansbu <hdle@cs.stonybrook.edu>
2020-02-14 14:50:01 -0500

    add ObjectPool

---
+++ src/CreationalPatterns/ObjectPool/ExportingProcess.java
@@ -0,0 +1,28 @@
+package CreationalPatterns.ObjectPool;
+
+public class ExportingProcess
+
+{
+	
+    private long processNo;
+
+    public ExportingProcess(long processNo) 
+    {
+         this.processNo = processNo;
+
+        // do some  expensive calls / tasks here in future
+        // .........
+     
+         
+      System.out.println("Object with process no. " + processNo + " was created");
+    
+    }
+
+   
+    public long getProcessNo() 
+    {
+        return processNo;
+    }
+
+  
+}// End of the ExportingProcess class.
---
+++ src/CreationalPatterns/ObjectPool/ExportingTask.java
@@ -0,0 +1,35 @@
+package CreationalPatterns.ObjectPool;
+
+public class ExportingTask implements Runnable
+{
+
+	    private ObjectPool<ExportingProcess> pool;
+
+	    private int threadNo;
+
+	    public ExportingTask(ObjectPool<ExportingProcess> pool, int threadNo) 
+	    {
+	        this.pool = pool;
+	        this.threadNo = threadNo;
+	    }
+
+	    
+	    public void run() 
+	    {
+	        // get an object from the pool
+	    	
+	        ExportingProcess exportingProcess = pool.borrowObject();
+
+	        System.out.println("Thread " + threadNo + ": Object with process no. " + exportingProcess.getProcessNo() + " was borrowed");
+
+	        //you can  do something here in future
+	        // .........
+	        
+
+            // return ExportingProcess instance back to the pool
+	        pool.returnObject(exportingProcess);
+
+	        System.out.println("Thread " + threadNo +": Object with process no. " + exportingProcess.getProcessNo() + " was returned");
+	    }
+
+}// End of the ExportingTask class.
